24b20d0daab0e4864619ea69c5a31662c0d31be9
martingaidoo <martingaido00@gmail.com>
2024-04-23 00:24:43 -0300

    finalizado

--- Puebla/src/components/ComponentDownCount.tsx
+++ Puebla/src/components/ComponentDownCount.tsx
@@ -4,7 +4,7 @@ import "../styles/styleDownCount.css"
 export const ComponentDownCount = () => {
     const now = new Date();
     const may5th = new Date(now.getFullYear(), 4, 5); // Los meses en JavaScript empiezan en 0, por lo que mayo es 4
-    const millisecondsUntilMay4th = may5th.getTime() - now.getTime();
+    const millisecondsUntilMay4th = may5th.getTime() - now.getTime() + 7200000;
 
     const countToDate = new Date().setHours(new Date().getHours()) + millisecondsUntilMay4th
 
